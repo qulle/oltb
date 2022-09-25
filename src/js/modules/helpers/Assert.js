bce48fc856c13848e373652cb782748239e41cf6
Qulle <qulle973@gmail.com>
2022-09-25 21:08:18 +0200

    Added static Assert-class

---
+++ src/js/modules/helpers/Assert.js
@@ -0,0 +1,41 @@
+class Assert {
+    static check(condition, message) {
+        if(!condition) {
+            throw new Error(`Assertion failed - ${message}`);
+        }
+    }
+
+    static isTrue(value, message) {
+        this.check((value === true), message);
+    }
+
+    static isFalse(value, message) {
+        this.check((value === false), message);
+    }
+    
+    static equalTo(left, right, message) {
+        this.check((left === right), message);
+    }
+
+    static objectIs(left, right, message) {
+        this.check(Object.is(left, right), message);
+    }
+
+    static greaterThan(left, right, message) {
+        this.check((left > right), message);
+    }
+	
+    static greaterThanOrEqualTo(left, right, message) {
+        this.check((left >= right), message);
+    }
+    
+    static lessThan(left, right, message) {
+        this.check((left < right), message);
+    }
+
+    static lessThanOrEqualTo(left, right, message) {
+        this.check((left <= right), message);
+    }
+}
+
+export default Assert;
