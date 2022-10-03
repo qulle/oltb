9a89588d802b62816e82a3a15752161ca1a0889c
Qulle <qulle973@gmail.com>
2022-10-03 22:06:19 +0200

    Added wind layer

---
+++ src/js/modules/helpers/ol-functions/GenerateWindbarb.js
@@ -0,0 +1,40 @@
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
+import { Icon, Style } from 'ol/style';
+import { fromLonLat } from 'ol/proj';
+import { getWindBarb } from '../../core/Windbarbs';
+
+const DEFAULT_OPTIONS = {
+    lat: undefined,
+    lon: undefined,
+    windSpeed: 0,
+    rotation: 0,
+    notSelectable: false,
+    infoWindow: undefined
+};
+
+const generateWindbarb = function(options = {}) {
+    options = { ...DEFAULT_OPTIONS, ...options };
+
+    const windbarb = new Feature({
+        geometry: new Point(fromLonLat([options.lon, options.lat]))
+    });
+
+    windbarb.setStyle([
+        new Style({
+            image: new Icon({
+                src: 'data:image/svg+xml;utf8,' + getWindBarb(options.windSpeed),
+                rotation: options.rotation
+            })
+        })
+    ]);
+
+    windbarb.setProperties({
+        notSelectable: options.notSelectable, 
+        infoWindow: options.infoWindow
+    });
+
+    return windbarb;
+}
+
+export { generateWindbarb };
---
+++ src/js/modules/layers/Wind.js
@@ -0,0 +1,68 @@
+import LayerManager from "../core/managers/LayerManager";
+import Toast from "../common/Toast";
+import { toStringHDMS } from "ol/coordinate";
+import { randomNumber } from "../helpers/Random";
+import { generateWindbarb } from "../helpers/ol-functions/GenerateWindbarb";
+
+import urlCapitalsGeoJSON from 'url:../../../json/capitals.geojson';
+
+const ID_PREFIX = 'oltb-info-window-marker';
+const LAYER_WRAPPER = LayerManager.addFeatureLayer('Windbarbs', true);
+
+const CONTINENT_DIRECTION = {
+    'Europe': 5,
+    'Africa': 0.17,
+    'Antarctica': 1,
+    'Asia': 0.5,
+    'Australia': 0.5,
+    'Central America': 1.57,
+    'North America': 1.57,
+    'South America': 1.57,
+    'UM': 1.57,
+    'US': 1.57
+};
+
+fetch(urlCapitalsGeoJSON)
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error(`Fetch error [${response.status}] [${response.statusText}]`);
+        }
+
+        return response.json();
+    })
+    .then((json) => {
+        json.features.forEach((capital) => {
+            const lon = capital.geometry.coordinates[0];
+            const lat = capital.geometry.coordinates[1];
+            const prettyCoords = toStringHDMS([lon, lat]);
+        
+            // Get SVG for winds between 0 and 40m/s (75knots)
+            const windSpeed = randomNumber(0, 40);
+            const rotation = CONTINENT_DIRECTION[capital.properties.continentName] || 0;
+
+            const infoWindow = `
+                <h3 class="oltb-text-center">${capital.properties.countryName} - ${capital.properties.countryCode}</h3>
+                <p class="oltb-text-center">Wind speed ${windSpeed}m/s - Direction ${rotation}deg</p>
+                <p class="oltb-text-center">${prettyCoords}</p>
+                <div class="oltb-d-flex oltb-justify-content-center">
+                    <button class="oltb-func-btn oltb-func-btn--delete oltb-tippy" title="Delete marker" id="${ID_PREFIX}-remove"></button>
+                    <button class="oltb-func-btn oltb-func-btn--copy oltb-tippy" title="Copy marker text" id="${ID_PREFIX}-copy-location" data-copy="Country ${capital.properties.countryName} Capital ${capital.properties.capitalName} Coordinates ${prettyCoords}"></button>
+                </div>
+            `;
+
+            LAYER_WRAPPER.layer.getSource().addFeature(
+                new generateWindbarb({
+                    lat: lat,
+                    lon: lon,
+                    windSpeed: windSpeed,
+                    rotation: rotation,
+                    notSelectable: true,
+                    infoWindow: infoWindow
+                })
+            );
+        });
+    })
+    .catch((error) => {
+        console.error(`Error loading Windbarbs [${error}]`);
+        Toast.error({text: 'Error loading Windbarbs layer'});  
+    });
