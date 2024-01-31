7046317a64da71f1cfc603ddb13001f1cf99db17
Qulle <qulle973@gmail.com>
2024-01-31 19:27:46 +0100

    New release v2.1.0

--- rollup.cssbanner.mjs
+++ rollup.cssbanner.mjs
@@ -1,8 +1,8 @@
 export const banner = `/*!
  * +-------------------------------------------------------------------------------+
  * | Project: OLTB                                                                 |
- * | Version: v2.0.0 https://github.com/qulle/oltb#readme                          |
- * |    Date: 2024-01-14 19:00                                                     |
+ * | Version: v2.1.0 https://github.com/qulle/oltb#readme                          |
+ * |    Date: 2024-01-31 20:00                                                     |
  * |  Author: Qulle https://github.com/qulle                                       |
  * | License: BSD-2-Clause license https://github.com/qulle/oltb/blob/main/LICENSE |
  * +-------------------------------------------------------------------------------+
