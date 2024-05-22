a9b4c858901cf4a3aa668c3a81bf8fd33ccf46ed
Qulle <qulle973@gmail.com>
2024-05-22 22:26:08 +0200

    Changed order of items in object

--- src/oltb/js/ol-mappers/ol-geometry.js
+++ src/oltb/js/ol-mappers/ol-geometry.js
@@ -2,16 +2,16 @@
 // Square and Rectangle are not official geometries
 // They are replaced with Circle-geometry at runtime
 const GeometryType = Object.freeze({
-    Square: 'Square',
-    Rectangle: 'Rectangle',
-    Point: 'Point',
     Circle: 'Circle',
-    Polygon: 'Polygon',
     LinearRing: 'LinearRing',
     LineString: 'LineString',
+    MultiLineString: 'MultiLineString',
     MultiPoint: 'MultiPoint',
     MultiPolygon: 'MultiPolygon',
-    MultiLineString: 'MultiLineString'
+    Point: 'Point',
+    Polygon: 'Polygon',
+    Rectangle: 'Rectangle',
+    Square: 'Square'
 });
 
 export { GeometryType };
