eb1a77d7315ef6d8cf255f5512323d294afe677c
Qulle <qulle973@gmail.com>
2024-06-16 22:25:49 +0200

    Added more tests, started adding base-tool-test, init of the tool.

--- src/oltb/js/toolbar-tools/bookmark-tool/bookmark-tool.test.js
+++ src/oltb/js/toolbar-tools/bookmark-tool/bookmark-tool.test.js
@@ -1,5 +1,62 @@
+import { jest, beforeAll, describe, it, expect } from '@jest/globals';
+import { BookmarkTool } from './bookmark-tool';
+import { StateManager } from '../../toolbar-managers/state-manager/state-manager';
+import { ElementManager } from '../../toolbar-managers/element-manager/element-manager';
+
+const FILENAME = 'bookmark-tool.js';
+const CLASS__TOOLBOX_SECTION = 'oltb-toolbox-section';
+const CLASS__TOOLBOX_LIST = 'oltb-toolbox-list';
+const ID__PREFIX = 'oltb-bookmark';
+const I18N__BASE = 'tools.bookmarkTool';
+const I18N__BASE_COMMON = 'commons';
+
+// Note:
+// Simplified mock from #initToolboxHTML
+const HTML__MOCK = (`
+    <div id="${ID__PREFIX}-toolbox" class="${CLASS__TOOLBOX_SECTION}">
+        <div class="${CLASS__TOOLBOX_SECTION}__header oltb-toggleable" data-oltb-toggleable-target="${ID__PREFIX}-toolbox-collapsed">
+            <h4 class="${CLASS__TOOLBOX_SECTION}__title" data-oltb-i18n="${I18N__BASE}.toolbox.titles.bookmarks">__JEST__</h4>
+            <span class="${CLASS__TOOLBOX_SECTION}__icon oltb-tippy" data-oltb-i18n="${I18N__BASE_COMMON}.titles.toggleSection" title="__JEST__"></span>
+        </div>
+        <div class="${CLASS__TOOLBOX_SECTION}__groups" id="${ID__PREFIX}-toolbox-collapsed" style="display: 'block'}">
+            <div class="${CLASS__TOOLBOX_SECTION}__group">
+                <div class="oltb-input-button-group">
+                    <input type="text" id="${ID__PREFIX}-add-text" class="oltb-input" data-oltb-i18n="${I18N__BASE}.toolbox.groups.addBookmark.placeholder" placeholder="__JEST__">
+                    <button type="button" id="${ID__PREFIX}-add-button" class="oltb-btn oltb-btn--green-mid oltb-tippy" data-oltb-i18n="${I18N__BASE}.toolbox.groups.addBookmark.add" title="__JEST__">+</button>
+                </div>
+            </div>
+            <div class="${CLASS__TOOLBOX_SECTION}__group">
+                <ul id="${ID__PREFIX}-stack" class="${CLASS__TOOLBOX_LIST}"></ul>
+            </div>
+        </div>
+    </div>
+`);
+
 describe('BookmarkTool', () => {
-    it('should be an empty test', () => {
-        expect(1).toEqual(1);
+    beforeAll(() => {
+        window.document.body.innerHTML = HTML__MOCK;
+
+        jest.spyOn(ElementManager, 'getToolbarElement').mockImplementation(() => {
+            return window.document.createElement('div');
+        });
+
+        jest.spyOn(ElementManager, 'getToolboxElement').mockImplementation(() => {
+            return window.document.createElement('div');
+        });
+
+        jest.spyOn(StateManager, 'getStateObject').mockImplementation(() => {
+            return {};
+        });
+
+        jest.spyOn(StateManager, 'setStateObject').mockImplementation(() => {
+            return;
+        });
+    });
+
+    it('should init the tool', () => {
+        const tool = new BookmarkTool({});
+
+        expect(tool).toBeTruthy();
+        expect(tool.getName()).toBe(FILENAME);
     });
 });
--- src/oltb/js/toolbar-tools/coordinates-tool/coordinates-tool.test.js
+++ src/oltb/js/toolbar-tools/coordinates-tool/coordinates-tool.test.js
@@ -1,5 +1,68 @@
+import { jest, beforeAll, describe, it, expect } from '@jest/globals';
+import { CoordinatesTool } from './coordinates-tool';
+import { StateManager } from '../../toolbar-managers/state-manager/state-manager';
+import { ElementManager } from '../../toolbar-managers/element-manager/element-manager';
+import { SettingsManager } from '../../toolbar-managers/settings-manager/settings-manager';
+
+const FILENAME = 'coordinates-tool.js';
+const CLASS__TOOLBOX_SECTION = 'oltb-toolbox-section';
+const ID__PREFIX = 'oltb-coordinates';
+const I18N__BASE = 'tools.coordinatesTool';
+const I18N__BASE_COMMON = 'commons';
+
+// // Note:
+// // Simplified mock from #initToolboxHTML
+const HTML__MOCK = (`
+    <div id="${ID__PREFIX}-toolbox" class="${CLASS__TOOLBOX_SECTION}">
+        <div class="${CLASS__TOOLBOX_SECTION}__header oltb-toggleable" data-oltb-toggleable-target="${ID__PREFIX}-toolbox-collapsed">
+            <h4 class="${CLASS__TOOLBOX_SECTION}__title" data-oltb-i18n="${I18N__BASE}.toolbox.titles.coordinates">__JEST__</h4>
+            <span class="${CLASS__TOOLBOX_SECTION}__icon oltb-tippy" data-oltb-i18n="${I18N__BASE_COMMON}.titles.toggleSection" title="__JEST__"></span>
+        </div>
+        <div class="${CLASS__TOOLBOX_SECTION}__groups" id="${ID__PREFIX}-toolbox-collapsed" style="display: 'block'}">
+            <div class="${CLASS__TOOLBOX_SECTION}__group">
+                <label class="oltb-label" for="${ID__PREFIX}-format" data-oltb-i18n="${I18N__BASE}.toolbox.groups.formats.title">__JEST__</label>
+                <select id="${ID__PREFIX}-format" class="oltb-select">
+                    <option value="DD" data-oltb-i18n="${I18N__BASE}.toolbox.groups.formats.dd">__JEST__</option>
+                    <option value="DMS" data-oltb-i18n="${I18N__BASE}.toolbox.groups.formats.dms">__JEST__</option>
+                </select>
+            </div>
+            <div class="${CLASS__TOOLBOX_SECTION}__group">
+                <label class="oltb-label" data-oltb-i18n="${I18N__BASE}.toolbox.groups.coordinates.title">__JEST__</em></label>
+                <table class="oltb-table oltb-table--horizontal oltb-table--no-background oltb-table--tight-bottom-and-top oltb-mt-05" id="${ID__PREFIX}-table"></table>
+            </div>
+        </div>
+    </div>
+`);
+
 describe('CoordinatesTool', () => {
-    it('should be an empty test', () => {
-        expect(1).toEqual(1);
+    beforeAll(() => {
+        window.document.body.innerHTML = HTML__MOCK;
+
+        jest.spyOn(ElementManager, 'getToolbarElement').mockImplementation(() => {
+            return window.document.createElement('div');
+        });
+
+        jest.spyOn(ElementManager, 'getToolboxElement').mockImplementation(() => {
+            return window.document.createElement('div');
+        });
+
+        jest.spyOn(StateManager, 'getStateObject').mockImplementation(() => {
+            return {};
+        });
+
+        jest.spyOn(StateManager, 'setStateObject').mockImplementation(() => {
+            return;
+        });
+
+        jest.spyOn(SettingsManager, 'addSetting').mockImplementation(() => {
+            return;
+        });
+    });
+
+    it('should init the tool', () => {
+        const tool = new CoordinatesTool({});
+
+        expect(tool).toBeTruthy();
+        expect(tool.getName()).toBe(FILENAME);
     });
 });
