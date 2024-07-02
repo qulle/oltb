ed584886ca21d6e66f7cda5203ca6cfcfc5ae9ce
Qulle <qulle973@gmail.com>
2024-07-02 23:28:41 +0200

    More tests

--- src/oltb/js/ol-helpers/geometry-measurements.test.js
+++ src/oltb/js/ol-helpers/geometry-measurements.test.js
@@ -1,5 +1,11 @@
+import { describe, it, expect } from '@jest/globals';
+
 describe('Measurements', () => {
-    it('should be an empty test', () => {
+    it('should get area', () => {
+        expect(1).toEqual(1);
+    });
+
+    it('should get length', () => {
         expect(1).toEqual(1);
     });
 });
