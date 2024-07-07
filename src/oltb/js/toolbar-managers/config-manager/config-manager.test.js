29db0e3b32c8f8571cb11f851fc28d91ad6baa00
Qulle <qulle973@gmail.com>
2024-07-07 15:27:19 +0200

    More tests

--- src/oltb/js/toolbar-managers/config-manager/config-manager.test.js
+++ src/oltb/js/toolbar-managers/config-manager/config-manager.test.js
@@ -1,5 +1,32 @@
+import { jest, describe, it, expect } from '@jest/globals';
+import axios from 'axios';
+import { ConfigManager } from './config-manager';
+
+const FILENAME = 'config-manager.js';
+
 describe('ConfigManager', () => {
-    it('should be an empty test', () => {
-        expect(1).toEqual(1);
+    it('should init the manager', async () => {
+        jest.spyOn(axios, 'get').mockImplementation(async () => {
+            return Promise.resolve({
+                status: 200,
+                data: '{}'
+            });
+        });
+
+        return ConfigManager.initAsync({}).then((result) => {
+            expect(result).toStrictEqual({
+                filename: FILENAME,
+                result: true
+            });
+        });
+    });
+
+    it('should have two overridden methods [setMap, getName]', () => {
+        const spy = jest.spyOn(ConfigManager, 'setMap');
+        const map = {};
+
+        ConfigManager.setMap(map);
+        expect(spy).toHaveBeenCalled();
+        expect(ConfigManager.getName()).toBe(FILENAME);
     });
 });
