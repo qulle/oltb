9a3e8c9a8ca3b96b7b03922563a23790e25b81f1
Qulle <qulle973@gmail.com>
2024-07-07 22:25:22 +0200

    More tests

--- src/oltb/js/toolbar-managers/config-manager/config-manager.test.js
+++ src/oltb/js/toolbar-managers/config-manager/config-manager.test.js
@@ -1,15 +1,22 @@
 import { jest, describe, it, expect } from '@jest/globals';
 import axios from 'axios';
+import { LogManager } from '../log-manager/log-manager';
 import { ConfigManager } from './config-manager';
 
 const FILENAME = 'config-manager.js';
 
 describe('ConfigManager', () => {
-    it('should init the manager', async () => {
+    it('should init the manager successful load [200 Ok]', async () => {
+        const config = {
+            localization: {
+                languages: []
+            }
+        };
+
         jest.spyOn(axios, 'get').mockImplementation(async () => {
             return Promise.resolve({
                 status: 200,
-                data: '{}'
+                data: JSON.stringify(config)
             });
         });
 
@@ -17,6 +24,26 @@ describe('ConfigManager', () => {
             expect(result).toStrictEqual({
                 filename: FILENAME,
                 result: true
+            });
+        });
+    });
+
+    it('should init the manager failed load [404 Not Found]', async () => {
+        const config = {};
+        const spy = jest.spyOn(LogManager, 'logWarning');
+
+        jest.spyOn(axios, 'get').mockImplementation(async () => {
+            return Promise.resolve({
+                status: 404,
+                data: JSON.stringify(config)
+            });
+        });
+
+        return ConfigManager.initAsync({}).then((result) => {
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(result).toStrictEqual({
+                filename: FILENAME,
+                result: false
             });
         });
     });
