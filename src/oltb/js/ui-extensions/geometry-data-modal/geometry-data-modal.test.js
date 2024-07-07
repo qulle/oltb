b5847382d60cb5814d0f0b2359f7e2120df2cdc9
Qulle <qulle973@gmail.com>
2024-07-07 22:51:15 +0200

    More tests

--- src/oltb/js/ui-extensions/geometry-data-modal/geometry-data-modal.test.js
+++ src/oltb/js/ui-extensions/geometry-data-modal/geometry-data-modal.test.js
@@ -12,9 +12,25 @@ describe('GeometryDataModal', () => {
     });
 
     it('should create modal-extension', () => {
-        const modal = new GeometryDataModal({
-            title: 'jest'
-        });
+        const options = {
+            title: 'jest',
+            data: {
+                id: 1234,
+                measurement: '3071640.38 km2',
+                oltbProperties: `<pre><code>{"type": "drawing"}</code></pre>`,
+                vertices: 6,
+                coordinates: `<pre><code>[
+                    [-2821600.331828734,8262006.348294033],
+                    [171346.63509037718,9719610.390624769],
+                    [890431.2959735394,7484617.525717639],
+                    [-333956.0995842796,5618884.351534298],
+                    [-2452340.6411049496,6337969.012417461],
+                    [-2821600.331828734,8262006.348294033]
+                ]</code></pre>`
+            }
+        };
+
+        const modal = new GeometryDataModal(options);
 
         expect(modal).toBeTruthy();
         expect(modal.getName()).toBe(FILENAME);
