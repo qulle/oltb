166d6758a0bb627147e6fc38d0179e481dcd588a
Qulle <qulle973@gmail.com>
2024-07-07 23:13:52 +0200

    More tests

--- src/oltb/js/browser-prototypes/json-cycle.test.js
+++ src/oltb/js/browser-prototypes/json-cycle.test.js
@@ -6,4 +6,34 @@ describe('JSONCycle', () => {
         expect(typeof JSON.decycle === 'function');
         expect(typeof JSON.retrocycle === 'function');
     });
+
+    it('should run [decycle]', () => {
+        const input = {
+            lon: 1234, 
+            lat: 4321, 
+            values: [0, 1, 2], 
+            items: {
+                a: 'foo', 
+                b: 'bar'
+            }
+        };
+        const output = JSON.stringify(JSON.decycle(input));
+
+        expect(output).toEqual('{"lon":1234,"lat":4321,"values":[0,1,2],"items":{"a":"foo","b":"bar"}}');
+    });
+
+    it('should run [decycle]', () => {
+        const input = '{"lon":1234,"lat":4321,"values":[0,1,2],"items":{"a":"foo","b":"bar"}}';
+        const output = JSON.retrocycle(JSON.parse(input));
+
+        expect(output).toStrictEqual({
+            lon: 1234, 
+            lat: 4321, 
+            values: [0, 1, 2], 
+            items: {
+                a: 'foo', 
+                b: 'bar'
+            }
+        });
+    });
 });
