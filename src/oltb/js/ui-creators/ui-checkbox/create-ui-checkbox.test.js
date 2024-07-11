93539ef286c4868b104c2549d6324c2420c80ef4
Qulle <qulle973@gmail.com>
2024-07-11 11:48:37 +0200

    More tests

--- src/oltb/js/ui-creators/ui-checkbox/create-ui-checkbox.test.js
+++ src/oltb/js/ui-creators/ui-checkbox/create-ui-checkbox.test.js
@@ -1,39 +1,100 @@
-import { beforeAll, describe, it, expect } from '@jest/globals';
+import { jest, describe, it, expect } from '@jest/globals';
 import { createUICheckbox } from './create-ui-checkbox';
+import { TranslationManager } from '../../toolbar-managers/translation-manager/translation-manager';
+
+const CLASS__CHECKBOX_WRAPPER = 'oltb-checkbox-wrapper';
 
 describe('CreateUICheckbox', () => {
-    let uiCheckbox = undefined;
-
-    beforeAll(() => {
-        uiCheckbox = createUICheckbox({
-            idPrefix: 'pre',
-            idPostfix: 'post',
-            text: 'Toggle setting',
-            checked: true
-        });
-    });
-      
     it('should create a ui-checkbox', () => {
+        const uiCheckbox = createUICheckbox();
+
         expect(uiCheckbox).toBeTruthy();
         expect(uiCheckbox.length).toBe(2);
     });
 
     it('should contain one DIV and one INPUT', () => {
+        const uiCheckbox = createUICheckbox({});
         const [ wrapper, checkbox ] = uiCheckbox;
+
         expect(wrapper.nodeName).toBe('DIV');
         expect(checkbox.nodeName).toBe('INPUT');
     });
 
     it('should be two childs in the wrapper of type INPUT and LABEL', () => {
+        const uiCheckbox = createUICheckbox({
+            bottomMargin: true
+        });
         const wrapper = uiCheckbox[0];
+
         expect(wrapper.childNodes.length).toBe(2);
         expect(wrapper.childNodes[0].nodeName).toBe('INPUT');
         expect(wrapper.childNodes[1].nodeName).toBe('LABEL');
+        expect(wrapper.classList.contains(`${CLASS__CHECKBOX_WRAPPER}--margin`)).toBe(true);
     });
 
     it('should be a checked checkbox', () => {
+        const uiCheckbox = createUICheckbox({
+            checked: true
+        });
         const checkbox = uiCheckbox[1];
+
         expect(checkbox.hasAttribute('checked')).toBe(true);
         expect(checkbox.getAttribute('checked')).toBe('');
+    });
+
+    it('should be translated using i18n', () => {
+        const spy = jest.spyOn(TranslationManager, 'get').mockImplementation(() => {
+            return 'jest-translated-value';
+        });
+
+        const uiCheckbox = createUICheckbox({
+            i18nKey: 'jest-key'
+        });
+        
+        const wrapper = uiCheckbox[0];
+        const label = wrapper.childNodes[1];
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(label.innerText).toBe('jest-translated-value');
+    });
+
+    it('should trigger one callback', () => {
+        const callbacks = {onClick: () => {}};
+        const spyOne = jest.spyOn(callbacks, 'onClick');
+        const uiCheckbox = createUICheckbox({
+            listeners: {
+                'click': callbacks.onClick
+            }
+        });
+
+        const checkbox = uiCheckbox[1];
+        checkbox.click();
+
+        expect(spyOne).toHaveBeenCalledTimes(1);
+    });
+
+    it('should trigger two callbacks', () => {
+        const callbacks = {
+            onClick: () => {},
+            onSecondClick: () => {}
+        };
+        
+        const spyOne = jest.spyOn(callbacks, 'onClick');
+        const spyTwo = jest.spyOn(callbacks, 'onSecondClick');
+
+        const uiCheckbox = createUICheckbox({
+            listeners: {
+                'click': [
+                    callbacks.onClick,
+                    callbacks.onSecondClick
+                ]
+            }
+        });
+
+        const checkbox = uiCheckbox[1];
+        checkbox.click();
+
+        expect(spyOne).toHaveBeenCalledTimes(1);
+        expect(spyTwo).toHaveBeenCalledTimes(1);
     });
 });
