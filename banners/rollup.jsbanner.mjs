25960598e98aacb0089adeae0f4c59ec15de4590
Qulle <qulle973@gmail.com>
2024-07-27 11:16:25 +0200

    Bumped packages

--- banners/rollup.jsbanner.mjs
+++ banners/rollup.jsbanner.mjs
@@ -7,34 +7,34 @@ export const banner = `/*!
  * | License: BSD-2-Clause license https://github.com/qulle/oltb/blob/main/LICENSE                                |
  * + -------------------------------------------------------------------------------------------------------------+
  * | Thanks to the following projects:                                                                            |
- * | (+) OpenLayers 9.1.0                   - https://openlayers.org                                              |
+ * | (+) OpenLayers 9.2.4                   - https://openlayers.org                                              |
  * | (+) proj4 2.11.0                       - http://proj4js.org                                                  |
  * | (+) Tippy.js 6.3.7                     - https://atomiks.github.io/tippyjs                                   |
  * | (+) Bootstrap Icons                    - https://icons.getbootstrap.com                                      |
  * | (+) Moment 2.30.1                      - https://momentjs.com                                                |
  * | (+) A Color Picker 1.2.1               - https://github.com/narsenico/a-color-picker                         |
  * | (+) Plain JS Slidetoggle 2.0.0         - https://github.com/ericbutler555/plain-js-slidetoggle               |
- * | (+) JSTS 2.11.2                        - https://github.com/bjornharrtell/jsts                               |
+ * | (+) JSTS 2.11.3                        - https://github.com/bjornharrtell/jsts                               |
  * | (+) Parcel 2.12.0                      - https://parceljs.org                                                |
  * | (+) @parcel/transformer-sass 2.12.0    - https://github.com/parcel-bundler/parcel                            |
- * | (+) Rollup 4.17.2                      - https://github.com/rollup/rollup                                    |
- * | (+) @rollup/plugin-commonjs 25.0.7     - https://github.com/rollup/plugins/tree/master/packages/commonjs     |
+ * | (+) Rollup 4.19.1                      - https://github.com/rollup/rollup                                    |
+ * | (+) @rollup/plugin-commonjs 26.0.1     - https://github.com/rollup/plugins/tree/master/packages/commonjs     |
  * | (+) @rollup/plugin-json 6.1.0          - https://github.com/rollup/plugins/tree/master/packages/json         |
  * | (+) @rollup/plugin-node-resolve 15.2.3 - https://github.com/rollup/plugins/tree/master/packages/node-resolve |
- * | (+) @rollup-plugin-replace 5.0.5       - https://github.com/rollup/plugins/tree/master/packages/replace      |
+ * | (+) @rollup-plugin-replace 5.0.7       - https://github.com/rollup/plugins/tree/master/packages/replace      |
  * | (+) @rollup/plugin-terser 0.4.4        - https://github.com/rollup/plugins/tree/master/packages/terser       |
- * | (+) rollup-plugin-license 3.3.1        - https://github.com/mjeanroy/rollup-plugin-license                   |
+ * | (+) rollup-plugin-license 3.5.2        - https://github.com/mjeanroy/rollup-plugin-license                   |
  * | (+) rollup-plugin-scss 4.0.0           - https://github.com/thgh/rollup-plugin-scss                          |
  * | (+) Cycle.js                           - https://github.com/douglascrockford/JSON-js                         |
  * | (+) Browser Dtector 4.1.0              - https://github.com/sibiraj-s/browser-dtector                        |
  * | (+) Sortable JS 1.15.2                 - https://github.com/SortableJS/Sortable                              |
- * | (+) UUID JS 9.0.1                      - https://github.com/uuidjs/uuid                                      |
+ * | (+) UUID JS 10.0.0                     - https://github.com/uuidjs/uuid                                      |
  * | (+) Lodash 4.17.21                     - https://github.com/lodash/lodash                                    |
  * | (+) Many Keys Map 2.0.1                - https://github.com/fregante/many-keys-map                           |
- * | (+) ESLint 9.2.0                       - https://github.com/eslint/eslint                                    |
+ * | (+) ESLint 9.8.0                       - https://github.com/eslint/eslint                                    |
  * | (+) @eslint/js 9.2.0                   - https://github.com/eslint/eslint                                    |
- * | (+) Husky 9.0.11                       - https://github.com/typicode/husky                                   |
- * | (+) Lint Staged 15.2.2                 - https://github.com/lint-staged/lint-staged                          |
+ * | (+) Husky 9.1.3                        - https://github.com/typicode/husky                                   |
+ * | (+) Lint Staged 15.2.7                 - https://github.com/lint-staged/lint-staged                          |
  * | (+) Jest 29.7.0                        - https://github.com/jestjs/jest                                      |
  * | (+) Jest Environment JSDOM 29.7.0      - https://github.com/jestjs/jest                                      |
  * | (+) JIdentity Obj Proxy 3.0.0          - https://github.com/keyz/identity-obj-proxy                          |
